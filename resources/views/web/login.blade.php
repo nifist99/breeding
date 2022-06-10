15df250a4955ebebfa6db38354db933fd0ebb205
nifist99 <nikofigit@gmail.com>
2022-06-10 22:14:21 +0700

    add vendor

--- resources/views/web/login.blade.php
+++ resources/views/web/login.blade.php
@@ -55,19 +55,12 @@
                                             Login
                                         </a>
                                         <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
                                     </form>
-                                    <hr>
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        <a class="small" href="{{url('forgot-password')}}">Forgot Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        <a class="small" href={{url('register')}}>Create an Account!</a>
                                     </div>
                                 </div>
                             </div>
--- resources/views/web/register.blade.php
+++ resources/views/web/register.blade.php
@@ -58,19 +58,12 @@
                                     Register Account
                                 </a>
                                 <hr>
-                                <a href="index.html" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Register with Google
-                                </a>
-                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                                </a>
                             </form>
-                            <hr>
                             <div class="text-center">
-                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                <a class="small" href="{{url('forgot-password')}}">Forgot Password?</a>
                             </div>
                             <div class="text-center">
-                                <a class="small" href="login.html">Already have an account? Login!</a>
+                                <a class="small" href="{{url('login')}}">Already have an account? Login!</a>
                             </div>
                         </div>
                     </div>
